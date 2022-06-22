7748807d5192d5d7d96ba8ac475edfb17586f5aa
basile <basile.pinsard@gmail.com>
2022-06-22 13:12:23 -0400

    make simple deploy tests to check autoenable

--- README.md
+++ README.md
@@ -1,16 +1,16 @@
-# Individual template action
+# Deploy tests
 
-This action updates the super-dataset with any changes to an individual template
+This action runs tests of datalad dataset deploy
 
 ## Inputs
 
-The action takes environment variables as inputs (`GITHUB_REPOSITORY`) and a secret access token (with write permissions to `templateflow/templateflow`)
+The action takes environment variables as inputs (`GITHUB_REPOSITORY`) and a secret access token (with write permissions to `courtois-neuromod` public repos)
 
 ## Example usage
 
 ```YAML
-name: "Update TemplateFlow's superdataset"
-uses: templateflow/actions-template@main
+name: "Deploy tests"
+uses: courtois-neuromod/actions-template@main
 env:
   SECRET_KEY: ${{ secrets.SUPER_SECRET_SSH_KEY }}
 ```
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,59 +1,21 @@
 #!/bin/bash
 
-git config --global user.name "$1"
-git config --global user.email "$2"
 
-# Create ~/.ssh folder
-mkdir -p /root/.ssh
-chmod 700 /root/.ssh
-
-# Add github and gin.g-node.org as trusted hosts
-ssh-keyscan -H github.com | install -m 600 /dev/stdin /root/.ssh/known_hosts
-ssh-keyscan -H gin.g-node.org >> /root/.ssh/known_hosts
-
-# Start ssh agent
-eval "$(ssh-agent -s)"
-
-# Add key to ssh agent
 ssh-add - <<< "${SECRET_KEY}"
+# Add github as trusted hosts
+ssh-keyscan -H github.com | install -m 600 /dev/stdin /root/.ssh/known_hosts
 
-echo "Installing TemplateFlow Archive super-dataset ..."
-datalad install git@github.com:templateflow/templateflow.git
+datalad install -r git@github.com:courtois-neuromod/${GITHUB_REPOSITORY##*/}.git
 
-echo "Installing template ${GITHUB_REPOSITORY##*/} ..."
-cd templateflow/
-git submodule set-url -- ${GITHUB_REPOSITORY##*/} git@github.com:templateflow/${GITHUB_REPOSITORY##*/}.git
-datalad install ${GITHUB_REPOSITORY##*/}
+ret_code=0
+# test autoenabled special remotes
+if ! $( git remote | grep -q -E '.*mri$' ) ; then
+  ret_code=1;
+fi
 
-echo "Updating template ${GITHUB_REPOSITORY##*/} ..."
-datalad update -d ${GITHUB_REPOSITORY##*/} --merge any .
+# test sensitive special remotes not autoenabled
+if $( git remote | grep -q -E '.*mri.sensitive$' ) ; then
+  ret_code=$((ret_code + 2));
+fi
 
-# Update GIN
-echo "Configuring g-Node/GIN remote ..."
-pushd ${GITHUB_REPOSITORY##*/}
-datalad siblings configure -d . --name gin \
-        --pushurl git@gin.g-node.org:/templateflow/${GITHUB_REPOSITORY##*/}.git \
-        --url https://gin.g-node.org/templateflow/${GITHUB_REPOSITORY##*/}
-git config --unset-all remote.gin.annex-ignore
-datalad siblings configure --name gin --as-common-datasrc gin-src
-datalad save -m "up: template action after content change"
-
-echo "Pushing to g-Node/GIN ..."
-datalad push --to gin .
-
-echo "Pushing to GitHub ..."
-datalad push --to origin .
-
-echo "Pushing super-dataset ..."
-popd
-git submodule set-url -- ${GITHUB_REPOSITORY##*/} https://github.com/templateflow/${GITHUB_REPOSITORY##*/}
-datalad save -m "update(${GITHUB_REPOSITORY##*/}): template action"
-datalad push --to origin .
-
-# Update S3
-echo "Exporting to S3 bucket ..."
-datalad siblings -d ${GITHUB_REPOSITORY##*/}/ enable -s s3
-pushd ${GITHUB_REPOSITORY##*/}
-datalad get -r *
-git annex export master --to s3
-popd
+return $ret_code
