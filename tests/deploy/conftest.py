1d82e1282a1c9df1c64978e21487cb4f1f5f314e
basile <basile.pinsard@gmail.com>
2022-12-02 11:16:51 -0500

    make ssh key import more robust

--- tests/deploy/conftest.py
+++ tests/deploy/conftest.py
@@ -19,9 +19,11 @@ def setup_git(
     subprocess.call(["sh", '-c', f"ssh-keyscan -H github.com | install -m 600 /dev/stdin /{os.environ['HOME']}/.ssh/known_hosts"])
 
     ssh_agent_setup.setup()
+    # ensure a single line return at the end of the key
+    secret_key = os.environ['SECRET_KEY'].rstrip('\n')+"\n"
     # add bot ssh key
-    process = subprocess.run(['ssh-add', '-'], input=os.environ['SECRET_KEY'].encode())
-    assert process.returncode == 0, 'fail to pass the ssh key to e'
+    process = subprocess.run(['ssh-add', '-'], input=secret_key.encode())
+    assert process.returncode == 0, 'fail to pass the ssh key to ssh-agent'
 
 
 @pytest.fixture(scope="session")
@@ -29,4 +31,4 @@ def dataset():
     ds = install(f"git@github.com:{os.environ['GITHUB_REPOSITORY']}.git")
     ds.repo.checkout(os.environ['GITHUB_SHA'])
     yield ds
-    ds.uninstall(check=False) #teardown
+    ds.uninstall(check=False, recursive=True) #teardown
