577b43c7e58416fa1728f3879990598da6180e60
bpinsard <basile.pinsard@gmail.com>
2024-05-27 15:40:55 -0400

    test submodules get with https cloned parent when ssh-git not setup

--- tests/deploy/conftest.py
+++ tests/deploy/conftest.py
@@ -13,12 +13,16 @@ logger = logging.getLogger(__name__)
 @pytest.fixture(scope="session", autouse=True)
 def setup_git(
         username=os.environ['GIT_USERNAME'],
-        email=os.environ['GIT_EMAIL']):
+        email=os.environ['GIT_EMAIL']
+    ):
 
     config = ConfigManager()
     config.set('user.name', username, scope='global')
     config.set('user.email', email, scope='global')
+    config.set('annex.security.allowed-ip-addresses', '10.10.10.20', scope='global')
 
+@pytest.fixture(scope="session")
+def setup_ssh():
     os.makedirs(os.path.join(os.environ['HOME'],'.ssh'), mode=700, exist_ok=True)
 
     subprocess.call(["sh", '-c', f"ssh-keyscan -H github.com | install -m 600 /dev/stdin /{os.environ['HOME']}/.ssh/known_hosts"])
@@ -32,11 +36,21 @@ def setup_git(
 
 
 @pytest.fixture(scope="session")
-def dataset(setup_git):
-    ds = install(f"git@github.com:{os.environ['GITHUB_REPOSITORY']}.git")
+def dataset(setup_git, setup_ssh):
+    yield from install_ds(protocol='ssh')
+
+def install_ds(protocol='ssh'):
+    url = f"git@github.com:{os.environ['GITHUB_REPOSITORY']}.git"
+    if protocol=='https':
+        url = f"https://github.com/{os.environ['GITHUB_REPOSITORY']}.git"
+    ds = install(path=f"ds_{protocol}", source=url)
     ds.repo.fetch('origin', os.environ['GITHUB_REF'])
     commitish = GIT_ANNEX_TEST_BRANCH if 'git-annex' in os.environ['GITHUB_REF_NAME'] else os.environ['GITHUB_SHA']
     logger.info(f"checking out {commitish}")
     ds.repo.checkout(commitish)
     yield ds
-    #ds.uninstall(check=False, recursive=True) #teardown
+    ds.drop(reckless='kill', recursive=True) #teardown
+
+@pytest.fixture(scope="function")
+def dataset_https():
+    yield from install_ds(protocol="https")
